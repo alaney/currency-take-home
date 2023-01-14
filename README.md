7b07ed6f73790e8496b21e6d2c65e2f7348bbf62
Aaron Laney <jaj.laney@gmail.com>
2023-01-14 17:21:47 -0600

    route to login page if not logged in

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Currency Take Home
+
+# Local Development
+
+## Dependencies
+
+[Node.js 16](https://nodejs.org/en/) or greater is required.
+
+## Setup
+
+From the root of the project run `npm ci` to install dependencies.
+
+After the setup is finished run `npm run dev` to launch the app in development mode. Visit http://localhost:3000 to view the application.
+
+# Design Decisions
+
+## Login
+
+I decided to create a `/login` route 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,31 @@
 import useAppStore from "@/useAppStore";
 import Head from "next/head";
-import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
   const store = useAppStore();
+  const router = useRouter();
 
-  if (!store.loggedIn) {
+  useEffect(() => {
+    if (!store.loggedIn) {
+      router.push("/login");
+    }
+  });
+
+  if (store.loggedIn) {
     return (
-      <div>
-        Sorry! You&apos;re not logged in. Click <Link href="/login">here</Link> to login.
-      </div>
+      <>
+        <Head>
+          <title>Currency Take Home</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <main>
+          <div>main</div>
+        </main>
+      </>
     );
+  } else {
+    return null;
   }
-
-  return (
-    <>
-      <Head>
-        <title>Currency Take Home</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main>
-        <div>main</div>
-      </main>
-    </>
-  );
 }
